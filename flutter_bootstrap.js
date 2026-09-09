d7af457cb8f2d95106fd036c9e3879d8af10fece
Enesdagci <enesdagci1287@hotmail.com>
2026-09-09 23:20:19 +0300

    changed the path names

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3882684783"
+    serviceWorkerVersion: "149297573"
   }
 });
